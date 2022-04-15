110f1d1981020719959fe2e6610300bfc687f0b8
정동욱 <dw.jung@mtwokorea.com>
2022-04-15 17:42:23 +0900

    정리

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+const calculator = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+  sub: function (a, b) {
+    console.log(a - b);
+  },
+  mult: function (a, b) {
+    console.log(a * b);
+  },
+  div: function (a, b) {
+    console.log(a / b);
+  },
+};
 
+calculator.add(10, 2);
+calculator.sub(10, 2);
+calculator.mult(10, 2);
+calculator.div(10, 2);
