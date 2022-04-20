49f78512471a443b3804a7c66f2113b82ce72978
정동욱 <dw.jung@mtwokorea.com>
2022-04-20 18:35:30 +0900

    220420by

--- app.js
+++ app.js
@@ -2,9 +2,24 @@ const loginForm = document.getElementById("login-form");
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
 
-function onLoginBtnClick(){
-  console.log(loginInput);
-  console.log("click!!!");
+const link = document.querySelector("a");
+
+function onLoginSubmit(toma) {
+  toma.preventDefault();
+  console.log(toma);
+
+  const username = loginInput.value;
+  console.log(username);
+  // if (username === "") {
+  //   alert("Please write your name");
+  // } else if (username.length > 15) {
+  //   alert("Your name is too long");
+  // }
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+function handleLinkClick() {
+  alert("clicked!");
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
