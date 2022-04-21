3f64787916c3fabc3936ef2f069ff1bbf4096169
정동욱 <dw.jung@mtwokorea.com>
2022-04-21 18:17:17 +0900

    220421

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 const loginForm = document.getElementById("login-form");
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 
-function onLoginSubmit(toma) {
-  toma.preventDefault();
-  console.log(toma);
+function onLoginSubmit(tomato) {
+  //submit 이벤트 감지
+  tomato.preventDefault(); //브라우저가 기본동작을 못 하도록 막아줌
+  console.log(tomato); // onLoginSubmit 이벤트 발생시 브라우저가 주는 정보, 이벤트에 관한 정보
 
   const username = loginInput.value;
+  //greeting.innerText = "Hello " + username;
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
   console.log(username);
   // if (username === "") {
   //   alert("Please write your name");
   // } else if (username.length > 15) {
   //   alert("Your name is too long");
   // }
+  loginForm.classList.add(HIDDEN_CLASSNAME);
 }
 
-function handleLinkClick() {
-  alert("clicked!");
-}
-
-loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
+loginForm.addEventListener("submit", onLoginSubmit); //실행시키면 handleLinkClick({이벤트에 관련된 정보}) <-오브젝트 추가됨(첫번째 인자)
