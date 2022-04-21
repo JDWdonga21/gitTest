121e0355a9d501ee1641c52aa0352bd1821ed658
jdwdonga21 <ongame1212121@naver.com>
2022-04-21 21:35:19 +0900

    220421cafe

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const loginButton = loginForm.querySelector("button");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(tomato) {
   //submit 이벤트 감지
@@ -11,9 +12,9 @@ function onLoginSubmit(tomato) {
   console.log(tomato); // onLoginSubmit 이벤트 발생시 브라우저가 주는 정보, 이벤트에 관한 정보
 
   const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
   //greeting.innerText = "Hello " + username;
-  greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  paintGreeting(username);
   console.log(username);
   // if (username === "") {
   //   alert("Please write your name");
@@ -22,5 +23,18 @@ function onLoginSubmit(tomato) {
   // }
   loginForm.classList.add(HIDDEN_CLASSNAME);
 }
+const saveUsername = localStorage.getItem(USERNAME_KEY);
+function paintGreeting(username){
+  greeting.innerText = `Hello ${ username }`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
+
+
+if(saveUsername === null){
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+}
+else{  
+  paintGreeting(saveUsername);
+}
 loginForm.addEventListener("submit", onLoginSubmit); //실행시키면 handleLinkClick({이벤트에 관련된 정보}) <-오브젝트 추가됨(첫번째 인자)
